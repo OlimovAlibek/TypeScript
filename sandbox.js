575b4002a0e8987604ce1889bdb5199b417b11ac
Alibek Olimov <2110101@newuzbekistanuniversity.uz>
2025-02-19 13:09:47 +0500

    first commit

---
+++ sandbox.js
@@ -0,0 +1,22 @@
+var character = 'mario';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circ(5));
+var char;
+var num;
+var done;
+var mixed = [];
+mixed.push('123');
+var mixed2 = [];
+mixed2.push(12);
+mixed2.push('adsasd');
+var obj;
+obj = { name: 'adasd' };
+var obj2;
