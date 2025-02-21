cba9b54188273a857f4fd88b606afa388c98294f
Alibek Olimov <2110101@newuzbekistanuniversity.uz>
2025-02-21 11:09:09 +0500

    new things learned

--- public/app.js
+++ public/app.js
@@ -1,10 +1,23 @@
-"use strict";
-const form = document.querySelector('.new-item-form');
-const type = document.querySelector('#type');
-const toFrom = document.querySelector('#tofrom');
-const details = document.querySelector('#details');
-const amount = document.querySelector('#amount');
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+import { Invoice } from './classes/invoice.js';
+// const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// const type = document.querySelector('#type') as HTMLSelectElement;
+// const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
+// const details = document.querySelector('#details') as HTMLInputElement;
+// const amount = document.querySelector('#amount') as HTMLInputElement;
+// form.addEventListener('submit', (e: Event) => {
+//     e.preventDefault()
+//     console.log(
+//         type.value,
+//         toFrom.value,
+//         details.value,
+//         amount.valueAsNumber
+//     );
+// })
+const invOne = new Invoice('Ali', 'website', 300);
+const invTwo = new Invoice('Nafisa', 'Finance', 200);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invoices.forEach(inv => {
+    console.log(inv.amount, inv.client, inv.format());
 });
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,37 @@
-const form = document.querySelector('.new-item-form') as HTMLFormElement;
+import {Invoice} from './classes/invoice.js'
 
-const type = document.querySelector('#type') as HTMLSelectElement;
-const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
-const details = document.querySelector('#details') as HTMLInputElement;
-const amount = document.querySelector('#amount') as HTMLInputElement;
+// const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
-form.addEventListener('submit', (e: Event) => {
-    e.preventDefault()
+// const type = document.querySelector('#type') as HTMLSelectElement;
+// const toFrom = document.querySelector('#tofrom') as HTMLInputElement;
+// const details = document.querySelector('#details') as HTMLInputElement;
+// const amount = document.querySelector('#amount') as HTMLInputElement;
 
-    console.log(
-        type.value,
-        toFrom.value,
-        details.value,
-        amount.valueAsNumber
-    );
+// form.addEventListener('submit', (e: Event) => {
+//     e.preventDefault()
+
+//     console.log(
+//         type.value,
+//         toFrom.value,
+//         details.value,
+//         amount.valueAsNumber
+//     );
     
-})
+// })
+
+
+
+
+const invOne = new Invoice('Ali', 'website', 300)
+const invTwo = new Invoice('Nafisa', 'Finance', 200)
+
+
+
+let invoices: Invoice[] = []
+invoices.push(invOne)
+invoices.push(invTwo)
+
+invoices.forEach(inv => {
+    console.log(inv.amount, inv.client, inv.format());
+    
+});
